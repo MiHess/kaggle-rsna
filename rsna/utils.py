96de6944efc530604cbe5e4df07a34aa8285f208
mirco.hess <mirco.hess@gmail.com>
2018-10-07 21:27:10 +0200

    changed to png

--- rsna/utils.py
+++ rsna/utils.py
@@ -154,8 +154,8 @@ class Annot:
 
 
         image_pil = PIL.Image.fromarray(image)
-        image_pil.save("tmp.jpg", "JPEG", quality=100)
-        fpath = os.path.join("tmp.jpg")
+        image_pil.save(f"tmp_{frame_id}.png", "PNG", quality=100)
+        fpath = os.path.join(f"tmp_{frame_id}.png")
         with open(fpath, "rb") as f:
             image_encoded = f.read()
 
